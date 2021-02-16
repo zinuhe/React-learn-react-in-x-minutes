8d9c76f1475cd7895bff56dea531b61777341a3d
Jimmy <zinuhe@outlook.com>
2021-02-15 19:34:43 -0500

    Added a checkbox for complete or not

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   //every todo in the list, a function to update the todos
   //const [todos, setTodos] = useState([])
   //const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
-  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false }])
+  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: true }])
 
   return (
     <>
