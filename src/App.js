de71dbeacfbeae18da800a9ccae70f6d138ee71b
Jimmy <zinuhe@outlook.com>
2021-02-15 19:29:32 -0500

    Added an object to the state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import TodoList from './TodoList'
 function App() {
   //every todo in the list, a function to update the todos
   //const [todos, setTodos] = useState([])
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  //const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false }])
+
   return (
     <>
       <TodoList todoList={todos} />
--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Todo({todo}) {
     return (
         <div>
-            {todo}
+            {todo.name}
         </div>
     )
 }
