5ad2b19b87011062b64abbd60608cacd9cc7061a
Jimmy <zinuhe@outlook.com>
2021-02-15 19:23:00 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import TodoList from './TodoList'
 
 function App() {
+  //every todo in the list, a function to update the todos
+  //const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todoList={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList({todoList}) {
+    return (
+        todoList.map(todo => {
+            return <Todo key={todo} todo={todo} />
+        })
+        
+        
+        //<div>
+        //    {todoList.length}
+        //</div>
+    )
+}
